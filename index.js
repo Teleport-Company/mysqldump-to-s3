58d18c4f99840337d6a795f1fa843c895be5fb2b
Tristan Kenney <tristan@kenney.co>
2017-01-18 11:10:19 +1100

    Log extra details on MySql connnection error

--- index.js
+++ index.js
@@ -1,56 +1,82 @@
-let aws = require('aws-sdk');
-let mysql = require('mysql');
-let moment = require('moment');
-let spawn = require('child_process').spawn;
+"use strict";
+const aws = require('aws-sdk');
+const mysql = require('mysql');
+const moment = require('moment');
+const limit = require('p-limit')(5);
+const spawn = require('child_process').spawn;
 
 
-exports.handler = function(event, context) {
-
+exports.handler = function(event, context, callback) {
+  process.env.EXPORT_S3_PATH = dateString(process.env.EXPORT_S3_PATH || '');
   let ignoredDatabases = ['mysql', 'performance_schema', 'innodb', 'information_schema'];
+  let connection = mysql.createConnection({
+    host: process.env.EXPORT_DB_HOST,
+    user: process.env.EXPORT_DB_USERNAME,
+    password: process.env.EXPORT_DB_PASSWORD,
+    connectTimeout: 2000
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      callback(`Error connecting to database: ${err} (host: ${process.env.EXPORT_DB_HOST}, user: ${process.env.EXPORT_DB_USERNAME})`);
+      return;
+    }
+  });
+
+  connection.query('SHOW DATABASES', function(err, databases) {
+    (databases || [])
+    .map((database) => database.Database)
+    .filter((database) => ignoredDatabases.indexOf(database) === -1)
+    .map(backupDatabase)
+    .forEach((promise) => {
+      promise.then((message) => console.log(message));
+      promise.catch((err) => console.error(err));
+    });
+  });
+
+  connection.end();
+}
+
+/**
+ *
+ * @param template
+ * @returns {string|XML}
+ */
+function dateString(template) {
   let date = moment();
-  process.env.EXPORT_S3_PATH = (process.env.EXPORT_S3_PATH || '')
-  .replace(/%YYYY/, date.format('YYYY'))
+  return template.replace(/%YYYY/, date.format('YYYY'))
   .replace(/%YY/, date.format('YY'))
   .replace(/%MM/, date.format('MM'))
   .replace(/%M/, date.format('M'))
   .replace(/%DD/, date.format('DD'))
   .replace(/%D/, date.format('D'));
-
-  let connection = mysql.createConnection({
-    host: process.env.EXPORT_DB_HOST,
-    user: process.env.EXPORT_DB_USERNAME,
-    password: process.env.EXPORT_DB_PASSWORD
-  });
-
-  connection.connect();
-  connection.query('SHOW DATABASES', function(err, databases, fields) {
-    databases.map((database) => database.Database)
-    .filter((database) => ignoredDatabases.indexOf(database) === -1)
-    .forEach(backupDatabase)
-  });
-  connection.end();
 }
 
-
+/**
+ * @param database
+ * @returns {Promise}
+ */
 function backupDatabase(database) {
-  let env = Object.create(process.env);
-  env.EXPORT_DB_NAME = database;
-  let backup = spawn('./export.sh', [], {env: env})
+  return limit(() => {
+    return new Promise(function(resolve, reject) {
 
-  backup.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
+      let env = Object.create(process.env);
+      env.EXPORT_DB_NAME = database;
+      console.log(`Exporting ${env.EXPORT_DB_NAME} to s3://${env.EXPORT_S3_PATH}/${env.EXPORT_DB_NAME}.gz`);
+      let backup = spawn('./export.sh', [], {env: env})
 
-  backup.stderr.on('data', (data) => {
-    console.error(data.toString());
-  });
+      backup.stderr.on('data', (data) => {
+        reject(data.toString());
+      });
 
-  backup.on('close', (code) => {
-    if (code === 0) {
-      console.log(`Successfully exported ${env.EXPORT_DB_NAME}`);
-    } else {
-      console.error(`Error exporting ${env.EXPORT_DB_NAME}`);
-    }
+      backup.on('close', (code) => {
+        if (code === 0) {
+          resolve(`Successfully exported ${env.EXPORT_DB_NAME}`);
+        } else {
+          reject(`Error exporting ${env.EXPORT_DB_NAME}`);
+        }
+      });
+    });
   });
 }
 
