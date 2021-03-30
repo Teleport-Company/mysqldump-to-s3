38e4342d8bafabc1c5b57b0914641b291cae755e
Alexandre Murphy-Gonthier <murphyalexandre@gmail.com>
2021-03-30 14:53:37 -0400

    Moved to mysqldump dependency.

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
-"use strict";
-const aws = require('aws-sdk');
-const mysql = require('mysql');
-const moment = require('moment');
-const limit = require('p-limit')(5);
-const spawn = require('child_process').spawn;
+const aws = require('aws-sdk')
+const { createReadStream } = require('fs')
+const moment = require('moment')
+const mysqldump = require('mysqldump')
 
+const s3 = new aws.S3()
 
-exports.handler = function(event, context, callback) {
-  process.env.EXPORT_S3_PATH = dateString(process.env.EXPORT_S3_PATH || '');
-  let ignoredDatabases = ['mysql', 'performance_schema', 'innodb', 'information_schema'];
-  let connection = mysql.createConnection({
-    host: process.env.EXPORT_DB_HOST,
-    user: process.env.EXPORT_DB_USERNAME,
-    password: process.env.EXPORT_DB_PASSWORD,
-    connectTimeout: 2000
-  });
+exports.handler = (event, context, callback) => {
+  return new Promise(async (resolve, reject) => {
+    const { EXPORT_DB_HOST, EXPORT_DB_NAME, EXPORT_DB_PASSWORD, EXPORT_DB_USERNAME, EXPORT_S3_BUCKET, EXPORT_S3_PATH } = process.env
 
-  connection.connect((err) => {
-    if (err) {
-      callback(`Error connecting to database: ${err} (host: ${process.env.EXPORT_DB_HOST}, user: ${process.env.EXPORT_DB_USERNAME})`);
-      return;
-    }
-  });
+    console.log(`Exporting DB ${EXPORT_DB_NAME} to /tmp/dump.sql.gz`)
 
-  connection.query('SHOW DATABASES', function(err, databases) {
-    (databases || [])
-    .map((database) => database.Database)
-    .filter((database) => ignoredDatabases.indexOf(database) === -1)
-    .map(backupDatabase)
-    .forEach((promise) => {
-      promise.then((message) => console.log(message));
-      promise.catch((err) => console.error(err));
-    });
-  });
+    mysqldump({
+      connection: {
+        host: EXPORT_DB_HOST,
+        user: EXPORT_DB_USERNAME,
+        password: EXPORT_DB_PASSWORD,
+        database: EXPORT_DB_NAME,
+      },
+      dump: {
+        data: {
+          format: false,
+        },
+      },
+      dumpToFile: '/tmp/dump.sql.gz',
+      compressFile: true,
+    })
+    .then(() => {
+      console.log('Create read stream to file /tmp/dump.sql.gz')
+      const readStream = createReadStream('/tmp/dump.sql.gz')
 
-  connection.end();
+      const key = dateString(`${EXPORT_S3_PATH}/${EXPORT_DB_NAME}.sql.gz`)
+      const params = {
+        Bucket: EXPORT_S3_BUCKET,
+        Key: key,
+        Body: readStream,
+      }
+
+      console.log(`Uploading dump to s3://${EXPORT_S3_BUCKET}/${key}`)
+
+      s3.putObject(params, (err, data) => {
+        if (err) {
+          console.error(err)
+          reject(err.toString())
+        } else {
+          resolve(data)
+        }
+      })
+    })
+    .catch((err) => {
+      console.error(err)
+      reject(err.toString())
+    })
+  })
 }
 
 /**
@@ -43,41 +61,11 @@ exports.handler = function(event, context, callback) {
  * @returns {string|XML}
  */
 function dateString(template) {
-  let date = moment();
+  let date = moment()
   return template.replace(/%YYYY/, date.format('YYYY'))
   .replace(/%YY/, date.format('YY'))
   .replace(/%MM/, date.format('MM'))
   .replace(/%M/, date.format('M'))
   .replace(/%DD/, date.format('DD'))
-  .replace(/%D/, date.format('D'));
+  .replace(/%D/, date.format('D'))
 }
-
-/**
- * @param database
- * @returns {Promise}
- */
-function backupDatabase(database) {
-  return limit(() => {
-    return new Promise(function(resolve, reject) {
-
-      let env = Object.create(process.env);
-      env.EXPORT_DB_NAME = database;
-      console.log(`Exporting ${env.EXPORT_DB_NAME} to s3://${env.EXPORT_S3_PATH}/${env.EXPORT_DB_NAME}.gz`);
-      let backup = spawn('./export.sh', [], {env: env})
-
-      backup.stderr.on('data', (data) => {
-        reject(data.toString());
-      });
-
-      backup.on('close', (code) => {
-        if (code === 0) {
-          resolve(`Successfully exported ${env.EXPORT_DB_NAME}`);
-        } else {
-          reject(`Error exporting ${env.EXPORT_DB_NAME}`);
-        }
-      });
-    });
-  });
-}
-
-
